1146cd0196936d3822fcc1a9fe8c4e038deb3f5b
Adrian-Mihai Onofrei <59933980+WoozyDeveloper@users.noreply.github.com>
2021-02-19 15:40:01 +0200

    Add files via upload

---
+++ Character.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Character : MonoBehaviour {
+
+    public Text scoreText;
+    private int points;
+    public Rigidbody2D rb;
+    public float moveSpeed;
+    public float flapHeight;
+    public GameObject pipe_up;
+    public GameObject pipe_down;
+
+    
+    // Use this for initialization
+    void Start () {
+        points = 0;
+        rb = GetComponent<Rigidbody2D>();
+        BuildLevel();
+    }
+
+    void BuildLevel()
+    {
+        Instantiate(pipe_down, new Vector3(14, 10), transform.rotation);
+        Instantiate(pipe_up, new Vector3(14, -10), transform.rotation);
+
+        Instantiate(pipe_down, new Vector3(22, 5), transform.rotation);
+        Instantiate(pipe_up, new Vector3(22, -15), transform.rotation);
+
+        Instantiate(pipe_down, new Vector3(34, 0), transform.rotation);
+        Instantiate(pipe_up, new Vector3(34, -20), transform.rotation);
+
+        Instantiate(pipe_down, new Vector3(46, 8), transform.rotation);
+        Instantiate(pipe_up, new Vector3(46, -12), transform.rotation);
+
+        Instantiate(pipe_down, new Vector3(57, 6), transform.rotation);
+        Instantiate(pipe_up, new Vector3(57, -14), transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, flapHeight);
+        }
+
+        
+
+        if (transform.position.y > 18 || transform.position.y <= -19)
+        {
+            Death();
+        }
+
+       
+       scoreText.text = "Score: " + points.ToString();
+        points++;
+    }
+
+
+    public void Death()
+    {
+        if (points > PlayerPrefs.GetInt("LevelHardScore"))
+        {
+            PlayerPrefs.SetInt("LevelHardScore", points);
+            PlayerPrefs.Save();
+        }
+
+        rb.velocity = Vector3.zero;
+        transform.position = new Vector2(0, 0);
+        BuildLevel();
+    }
+}
---
+++ backRBG.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class backRBG : MonoBehaviour {
+    Camera image;
+    byte cont = 10, cont2 = 5, cont3 = 3;
+	// Use this for initialization
+	void Start () {
+        image=GetComponent<Camera>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (cont <= 255)
+            cont += 10;
+        else
+            cont -= 10;
+        if (cont2 <= 255)
+            cont2 += 5;
+        else
+            cont2 -= 5;
+        if (cont3 <= 255)
+            cont3 += 3;
+        else
+            cont3 -= 3;
+        image.backgroundColor = new Color32(cont, cont2, cont3, 100);
+    }
+}
